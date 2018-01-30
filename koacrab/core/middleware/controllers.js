28c721a14c5ea67927ba9fa6011c9c6a92948117
haizlin <80285586@qq.com>
2018-01-30 17:17:01 +0800

    删除utils

--- koacrab/core/middleware/controllers.js
+++ koacrab/core/middleware/controllers.js
@@ -4,7 +4,7 @@
  */
 const fs = require('fs');
 const path = require('path');
-const utils = require('koacrab-utils');
+const utils = require('../utils');
 const config = require('../config/index.js');
 
 module.exports = function() {
